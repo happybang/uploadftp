d503bacef5ccce4f387b7c98e57af1cb1a1d785f
小飞侠 <shixi_huyue3@staff.sina.com.cn>
2018-01-24 10:56:06 +0800

    v0.0.3

---
+++ uploadftp.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+let ftp = require("./ftp");
+let fs = require("fs");
+let path = require("path");
+
+let cwd = process.cwd();
+
+module.exports = option => {
+  if (!option) {
+    option = require(path.resolve(cwd, "./uploadftpOption"));
+  }
+
+  let upload = async (src, dest) => {
+    console.log("from: ", src);
+    console.log("to: ", dest);
+    let name = path.basename(src);
+    if (fs.statSync(src).isDirectory()) {
+      await ftp.mkdir(path.resolve(dest, name));
+      let files = fs.readdirSync(src);
+      for (let i = 0; i < files.length; ++i) {
+        let file = files[i];
+        await upload(path.resolve(src, file), path.resolve(dest, name));
+      }
+    } else {
+      await ftp.put(src, path.resolve(dest, name));
+    }
+  };
+
+  let uploadftp = async () => {
+    if (!option.src || !option.dest) {
+      console.error("you have not src/dest option in your uploadOption.js !");
+      return;
+    }
+    let src = path.resolve(cwd, option.src);
+    let dest = option.dest;
+    if (!fs.existsSync(src)) {
+      console.error(`you have not ${src} in your computer !`);
+      return;
+    }
+
+    await ftp.connect(option);
+    console.log("uploadftp connect !");
+
+    await upload(src, dest);
+
+    await ftp.end();
+    console.log("uploadftp end !");
+  };
+
+  uploadftp();
+};
